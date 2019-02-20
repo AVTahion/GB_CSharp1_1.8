4cd817559e4331a16dae6bf103becc042261eb86
AVTahion <avkontakt40@gmail.com>
2019-02-21 00:59:57 +0300

    Задание 3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,12 +18,14 @@ using System.Windows.Forms;
 
 namespace Task_3
 {
-    static class Program
+    static partial class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
+    
+
+    /// <summary>
+    /// Главная точка входа для приложения.
+    /// </summary>
+    [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
